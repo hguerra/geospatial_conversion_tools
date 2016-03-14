cb6f48e3b0a2ca910c4ff79baae175de7df91a9f
Heitor Carneiro <heitorguerrac@gmail.com>
2016-03-14 16:26:01 -0300

    add social classes 2010 real

--- raster_info.R
+++ raster_info.R
@@ -8,8 +8,9 @@ library(rgdal)
 #shp.name <- "classes_sociais_2010/caragua_classes2010.shp"
 shp.name <- "classes_sociais_2010/regions.shp"
 
-if(!file.exists(shp.name))
-  return;
+if (!file.exists(shp.name))
+  return
+
 # Get the layer names
 shp.layers <- ogrListLayers(dsn = shp.name)
 # Load the file for the first layer
@@ -18,8 +19,9 @@ shp.extent <- extent(shp.data)
 #--------------------raster---------------------------
 raster.name <- "classes_sociais_2010/editar.tif"
 
-if(!file.exists(raster.name))
-  return;
+if (!file.exists(raster.name))
+  return
+
 
 rs <- raster(raster.name)
 rs.extent.old <- extent(rs)
@@ -32,10 +34,16 @@ rs.extent.new
 
 #---------------------write---------------------------
 #extent      : -45.48924, -45.25525, -23.745, -23.539(xmin, xmax, ymin, ymax)
-raster.extent <- c(-45.48924, -45.25525, -23.745, -23.539)
+raster.extent <- c(-45.48924,-45.25525,-23.745,-23.539)
 extent(rs) <- raster.extent
-writeRaster(rs,
-            "classes_sociais_2010/caragua_classes2010_regioes.tif",
-            format = 'GTiff',
-            overwrite = TRUE)
 
+writeRaster(rs,
+            "classes_sociais_2010/teste_caragua_classes2010_regioes.tif",
+            format = 'GTiff',datatype='INT2U', overwrite=TRUE)
+
+# grd <- readGDAL("classes_sociais_2010/caragua_classes2010_regioes.tif")
+# 
+# writeGDAL(grd,
+#           fname = "classes_sociais_2010/float32_caragua_classes2010_regioes.tif",
+#           drivername = "GTiff",
+#           type = "Float32")
