dc605f48263d02eff56bafef68eefd352e337d5e
hguerra <heitorguerrac@gmail.com>
2016-03-18 19:02:45 -0300

    edit

--- png2raster-classes_sociais.R
+++ png2raster-classes_sociais.R
@@ -1,22 +1,36 @@
 ##############################################################
 ####################SetEnvironment###########################
+## data path
+setwd("data")
 # librarys
+library(rgdal)
 library(raster)
 library(png)
 #start time
 strt<-Sys.time()
 ##############################################################
 #####################Variables################################
-# Classe social
+# Colors
+# Get color values:(col2rgb(color)/255)
+# ex: 
+# color.red <- rgb(255, 0, 0, maxColorValue = 255)
+# rgb.red <- col2rgb(color.red)/255
+color.classe.a.red <- 1
+color.classe.a.green <- 1
+color.classe.a.blue <- 0
+color.classe.b.red <- 1.0000000
+color.classe.b.green <- 0.5098039
+color.classe.b.blue <- 0.0000000
+color.classe.c.red <- 1
+color.classe.c.green <- 0
+color.classe.c.blue <- 0
+# Raster values
 classe.a <- 3
 classe.b <- 2
 classe.c <- 1
-color.red <- rgb(255, 0, 0, maxColorValue = 255)#C
-color.orange <- rgb(255, 130, 0, maxColorValue = 255)#B
-color.yellow <- rgb(255, 255, 0, maxColorValue = 255)#A
-# Raster
-img.filepath <- "SIMULACAO2025_BASELINE.png"
-raster.name <- "int16_simulacao2025_baseline.tif"
+# Raster/Image
+img.filepath <- "ignore_data/INITIAL2010_REALDATA_100.PNG"
+raster.name <- "int16_initial.tif"
 raster.proj <-
   "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 #extent      : -45.48924, -45.25525, -23.745, -23.539(xmin, xmax, ymin, ymax)
@@ -32,14 +46,16 @@ raster.img <- raster(nrow = nrow(img), ncol = ncol(img))
 print("Iniciando...")
 for (i in 1:nrow(img)) {
   for (j in 1:ncol(img)) {
-    color.actual <- rgb(img[i, j, 1], img[i, j, 2], img[i, j, 3])
-    if (color.actual == color.red) {
+    color.red <- img[i, j, 1]
+    color.green <- img[i, j, 2]
+    color.blue <- img[i, j, 3]
+    if(color.red == color.classe.c.red && color.green == color.classe.c.green && color.blue == color.classe.c.blue){
       raster.img[i, j] <- classe.c
-    } else if (color.actual == color.orange) {
+    }else if(color.red == color.classe.b.red && color.green == color.classe.b.green && color.blue == color.classe.b.blue){
       raster.img[i, j] <- classe.b
-    } else if (color.actual == color.yellow) {
+    }else if(color.red == color.classe.a.red && color.green == color.classe.a.green && color.blue == color.classe.a.blue){
       raster.img[i, j] <- classe.a
-    } else{
+    }else{
       raster.img[i, j] <- NA
     }
   }
